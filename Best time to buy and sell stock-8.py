b5fda3423de42f8a67bef59ae7f89ce5f5dd759a
Shaqil Shaheen <91945855+ShaqilShaheen10@users.noreply.github.com>
2023-07-15 21:10:45 +0530

    Create Best time to buy and sell stock-8.py

---
+++ Best time to buy and sell stock-8.py
@@ -0,0 +1,47 @@
+Best time to buy and sell stock-8
+
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+Input Format
+First line of each test case or query contains an integer 'N' representing the size of the first array/list.
+Second line contains 'N' single space separated integers representing the elements in the array/list.
+
+Output Format
+Integer representing the answer of the problem statement.
+Sample Input 0
+6
+7 1 5 3 6 4
+  
+Sample Output 0
+5
+  
+Explanation 0
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+  
+Sample Input 1
+5
+7 6 4 3 1
+
+Sample Output 1
+0
+
+Python Code:
+
+def max_profit(prices):
+    if len(prices) < 2:
+        return 0
+    min_price = prices[0]
+    max_profit = 0
+    for price in prices[1:]:
+        if price < min_price:
+            min_price = price
+        elif price - min_price > max_profit:
+            max_profit = price - min_price
+    return max_profit
+n = int(input())
+prices = list(map(int, input().split()))
+result = max_profit(prices)
+print(result)
