5eba048853f93d0bfbf24e12fd735bc46c9e783f
Shaqil Shaheen <91945855+ShaqilShaheen10@users.noreply.github.com>
2023-07-23 20:27:27 +0530

    Update Control Statements - Collatz problem.py

--- Control Statements - Collatz problem.py
+++ Control Statements - Collatz problem.py
@@ -1,5 +1,7 @@
 Control Statements - Collatz problem
-Chander started working for Bing and he wanted him to write a program to generate Collatz Sequence. The rules for generating the Collatz sequence are: If n is even,
+
+Chander started working for Bing and he wanted him to write a program to generate Collatz Sequence. 
+The rules for generating the Collatz sequence are: If n is even,
 n = n / 2. If n is odd, n = 3n + 1.
 For example, if the starting number is 5 the sequence is: 5 -> 16 -> 8 -> 4 -> 2 -> 1 It has been proved for almost all integers, that the repeated application
 of the above rule will result in a sequence that ends in 1.
